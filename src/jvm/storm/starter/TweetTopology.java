b24ea9c8b5f4b4433288c5813b132138c069a4b1
Lewis Kaneshiro <lewis@udacity.com>
2014-07-16 19:17:25 -0700

    storm-hack code with modifications to share with VM

---
+++ src/jvm/storm/starter/TweetTopology.java
@@ -0,0 +1,80 @@
+package storm.starter;
+
+import backtype.storm.Config;
+import backtype.storm.LocalCluster;
+import backtype.storm.StormSubmitter;
+import backtype.storm.task.OutputCollector;
+import backtype.storm.task.TopologyContext;
+import backtype.storm.testing.TestWordSpout;
+import backtype.storm.topology.OutputFieldsDeclarer;
+import backtype.storm.topology.TopologyBuilder;
+import backtype.storm.topology.base.BaseRichBolt;
+import backtype.storm.tuple.Fields;
+import backtype.storm.tuple.Tuple;
+import backtype.storm.tuple.Values;
+import backtype.storm.utils.Utils;
+import storm.starter.spout.TwitterSampleSpout;
+
+
+import java.util.Map;
+
+/**
+ * This is a basic example of a Storm topology.
+ */
+public class TweetTopology {
+
+  public static class StdoutBolt extends BaseRichBolt {
+    OutputCollector _collector;
+
+    @Override
+    public void prepare(Map conf, TopologyContext context, OutputCollector collector) {
+      _collector = collector;
+    }
+
+    @Override
+    public void execute(Tuple tuple) {
+      _collector.emit(tuple, new Values(tuple.getValue(0)));
+      _collector.ack(tuple);
+    }
+
+    @Override
+    public void declareOutputFields(OutputFieldsDeclarer declarer) {
+      declarer.declare(new Fields("word"));
+    }
+  }
+
+  public static void main(String[] args) throws Exception {
+    TopologyBuilder builder = new TopologyBuilder();
+
+    builder.setSpout("word", 
+           //new TwitterSampleSpout("[Your customer key]",
+           //     "[Your secret key]",
+           //    "[Your access token]", 
+           //    "[Your access secret]"), 1);
+               new TwitterSampleSpout(
+                "1sh8vxoGt0PffqUfEaAvXwLb7",
+                 "TUGNy6K7NpXieEt8x6coKBJEYa8MTyTWVDJqwNRR3vWI22ZlgO",
+                 "2516182968-SEWJ7w7vsVrGYIE4ea1wiXwdJ8ExuySmFcKsbTn", 
+                 "YhxRWjzKzlXNvlkFyC4561WFkSbTWjbRaO1o3oVTaklIj"), 
+               1);
+
+    builder.setBolt("stdout", new StdoutBolt(), 3).shuffleGrouping("word");
+
+    Config conf = new Config();
+    conf.setDebug(true);
+
+    if (args != null && args.length > 0) {
+      conf.setNumWorkers(3);
+
+      StormSubmitter.submitTopology(args[0], conf, builder.createTopology());
+    }
+    else {
+
+      LocalCluster cluster = new LocalCluster();
+      cluster.submitTopology("test", conf, builder.createTopology());
+      Utils.sleep(100000);
+      cluster.killTopology("test");
+      cluster.shutdown();
+    }
+  }
+}
